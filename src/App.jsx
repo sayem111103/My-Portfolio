89423c7e5be12a0733c835ec14ca78ddea7764e5
sayem111103 <sayemul03@gmail.com>
2023-07-02 20:52:35 +0600

    messenger integrated

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import './App.css'
 // import { mouse_context } from './CursorContext/CursorContext'
 import { RouterProvider } from 'react-router-dom';
 import router from './Routes/Routes';
+import MessengerCustomerChat from 'react-messenger-customer-chat';
+
 
 function App() {
   // const { variants, cursorVariant } = useContext(mouse_context);
@@ -12,6 +14,10 @@ function App() {
     <>
       {/* <motion.div variants={variants} animate={cursorVariant} className='cursor'></motion.div> */}
         <RouterProvider router={router} />
+        <MessengerCustomerChat
+            pageId={`${import.meta.env.VITE_PAGEID}`}
+            appId={`${import.meta.env.VITE_APPID}`}
+        />
     </>
   )
 }
